90b53dba2f19e8e06f630e3e9f73f330822efffe
Juan Carlos Benavides <juankbenavidess@gmail.com>
2020-06-02 18:12:54 -0500

    Creacion de reglas de negocio de gestion y sesion

---
+++ src/main/java/ec/edu/ups/negocio/ProcesoGestionRemotoON.java
@@ -0,0 +1,49 @@
+package ec.edu.ups.negocio;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import ec.edu.ups.modelos.Credito;
+import ec.edu.ups.modelos.Cuenta;
+import ec.edu.ups.modelos.Persona;
+import ec.edu.ups.modelos.SolicitudCredito;
+import ec.edu.ups.modelos.Transaccion;
+import ec.edu.ups.modelos.Usuario;
+
+/**
+ * Esta inteface define métodos útiles
+ * para el proceso de gestión del Banco. 
+ */
+public interface ProcesoGestionRemotoON {
+
+	/**
+	 * Registra el cliente especificado en el banco.
+	 **/
+	public void registrarUsuario(Persona cliente, Usuario usuario) throws Exception;
+	
+	/**
+	 * Devuelve la lista completa de usuarios registrados.
+	 **/
+	public List<Usuario> listarUsuarios();
+
+	/**
+	 * Devuelve la lista completa de cuentas abiertas.
+	 **/
+	public List<Cuenta> listarCuentas();
+	
+	/**
+	 * Devuelve la lista completa de solicitudes de credito registradas.
+	 **/
+	public List<SolicitudCredito> listarSolicitudesCredito();
+	
+	/**
+	 * Devuelve la lista completa de creditos registrados..
+	 **/
+	public List<Credito> listarCreditos();
+	
+	/**
+	 * Devuelve la lista de movimientos realizada dentro del rango de fechas especificado.
+	 **/
+	public List<Transaccion> listarMovimientos(Cuenta cuenta, LocalDate fechaInicio, LocalDate fechaFin);
+
+}
---
+++ src/main/java/ec/edu/ups/negocio/ProcesoSesionRemotaON.java
@@ -0,0 +1,44 @@
+package ec.edu.ups.negocio;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import ec.edu.ups.modelos.Cuenta;
+import ec.edu.ups.modelos.Transaccion;
+import ec.edu.ups.modelos.Usuario;
+
+/**
+ * Esta interface define métodos útiles 
+ * para el proceso de sesión de un usuario.
+ **/
+@Remote
+public interface ProcesoSesionRemotaON {
+
+	/**
+	 * Busca el usuario asociado con el correo especificado.
+	 **/
+	public Usuario buscarUsuario(String correo) throws Exception;
+	
+	/**
+	 * Devuelve un valor booleano indicando si las credenciales son validas.<br><br>
+	 **/
+	public boolean validarCredenciales(String correo, String clave) throws Exception;
+	
+	/**
+	 * Genera una nueva clave para el usuario.
+	 **/
+	public void cambiarClave(Usuario usuario) throws Exception;
+	
+	/**
+	 * Envia una notificación al usuario sobre el inicio de sesión.
+	 **/
+	public String notificarIntentoSesion(Usuario usuario, boolean exitoso) throws Exception;
+	
+	/**
+	 * Registra el intento de inicio de sesión del usuario.
+	 **/
+	public void registrarIntentoSesion(Usuario usuario, boolean exitoso) throws Exception;
+	
+}
