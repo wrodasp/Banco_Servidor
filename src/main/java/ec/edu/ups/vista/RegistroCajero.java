2a0294eb7946fe36fcd9fca87f4e1551605e1e28
Juan Carlos Benavides <juankbenavidess@gmail.com>
2020-06-05 14:45:08 -0500

    Creacion de ventana para deposito

---
+++ src/main/java/ec/edu/ups/vista/RegistroCajero.java
@@ -0,0 +1,70 @@
+package ec.edu.ups.vista;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import ec.edu.ups.modelos.Cuenta;
+import ec.edu.ups.modelos.Transaccion;
+import ec.edu.ups.negocio.ProcesoCajeroLocalON;
+import ec.edu.ups.negocio.ProcesoGestionLocalON;
+
+@ManagedBean
+public class RegistroCajero 
+{
+	@Inject
+	private ProcesoGestionLocalON procesoGestion;
+	
+	@Inject
+	private ProcesoCajeroLocalON procesoCajero;
+	
+	private Cuenta cuenta;
+	
+	private Transaccion transaccion;
+	
+	@PostConstruct
+	public void init()
+	{
+		cuenta = new Cuenta();
+		transaccion = new Transaccion();
+	}
+
+	public Cuenta getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(Cuenta cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public Transaccion getTransaccion() {
+		return transaccion;
+	}
+
+	public void setTransaccion(Transaccion transaccion) {
+		this.transaccion = transaccion;
+	}
+	
+	public String obtenerCuenta()
+	{
+		cuenta= procesoCajero.buscarCuenta(cuenta.getId());
+		System.out.println(cuenta.getPropietario());
+		return null;
+	}
+	
+	
+	public String realizaDeposito()
+	{
+		try {
+			procesoCajero.depositar(cuenta, transaccion.getMonto());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+
+}
